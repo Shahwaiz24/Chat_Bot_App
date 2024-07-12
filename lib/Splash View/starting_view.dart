7a30960c9534e3e84cb16cbf386a2117b59574ec
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-07-11 22:24:12 -0700

    Chat Bot App

---
+++ lib/Splash View/starting_view.dart
@@ -0,0 +1,16 @@
+import 'package:chat_bot/Splash%20View/starting_viewModel.dart';
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+
+class StartingView extends StatelessWidget {
+  const StartingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  ViewModelBuilder<StartingViewmodel>.reactive(viewModelBuilder: () => StartingViewmodel(), builder: (context, viewModel, child)  {
+      return Scaffold(
+    );
+    }
+    );
+  }
+}
---
+++ lib/Splash View/starting_viewModel.dart
@@ -0,0 +1,5 @@
+import 'package:stacked/stacked.dart';
+
+class StartingViewmodel extends BaseViewModel{
+  
+}
