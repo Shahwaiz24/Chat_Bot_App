92b3258d91b88bac29ca4bb98705d38987ab8aac
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-07-12 10:38:30 -0700

    Updated

--- lib/Services/utils.dart
+++ lib/Services/utils.dart
@@ -4,4 +4,6 @@ class Utils {
   static Color TextColor = Color(0xffFFFFFF);
   static Color TextGreyColor = Color(0xffC3C3C3);
   static Color backgroundColor = Color(0xff242424);
+  static Color Purple = Color(0xff9643FF);
+  static Color Pink = Color(0xffB384FF);
 }
--- lib/SignUp/signup_view.dart
+++ lib/SignUp/signup_view.dart
@@ -16,6 +16,7 @@ class _SignupViewState extends State<SignupView> {
     // Get Size
     final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth = MediaQuery.of(context).size.width;
+
     return ViewModelBuilder.reactive(
       viewModelBuilder: () => SignupViewmodel(),
       builder: (context, viewModel, child) {
@@ -26,25 +27,67 @@ class _SignupViewState extends State<SignupView> {
               Container(
                 height: screenHeight,
                 width: screenWidth,
-                child: Image(
+                child: const Image(
                   image: AssetImage('assets/images/background_2.png'),
                   fit: BoxFit.cover,
                 ),
               ),
               Column(
                 children: [
-                  SizedBox(
-                      height: screenHeight *
-                          0.050), // Add some padding from the top if needed
-                  Padding(
-                    padding: EdgeInsets.only(left: screenWidth * 0.040),
-                    child: Icon(
-                      Icons.arrow_back_outlined,
-                      size: screenHeight * 0.035,
-                      color: Utils.TextColor,
-                    ),
+                  SizedBox(height: screenHeight * 0.050),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(width: screenWidth * 0.05),
+                      Icon(
+                        Icons.arrow_back_outlined,
+                        size: screenHeight * 0.035,
+                        color: Utils.TextColor,
+                      ),
+                    ],
                   ),
-                  
+                  Spacer(),
+                  ClipRRect(
+                    clipBehavior: Clip.antiAlias,
+                    borderRadius: BorderRadius.circular(
+                        0), // or any other radius you want
+                    child: Stack(
+                      
+                      children: [
+                        // Your gradient container
+                        Container(
+                          height: screenHeight * 0.500,
+                          width: screenWidth,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(screenWidth * 0.32),
+                              topRight: Radius.circular(screenWidth * 0.32),
+                            ),
+                            gradient: LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              colors: [
+                                Utils.Purple.withOpacity(0.4),
+                                Utils.Pink.withOpacity(0.4)
+                              ],
+                            ),
+                          ),
+                        ),
+                        // Your positioned image
+                        Positioned(
+                          top: -screenHeight * 0.040,
+                          child: Container(
+                            height: screenHeight * 0.070,
+                            width: screenWidth * 0.250,
+                            child: Image(
+                              image: AssetImage('assets/images/animIcon.png'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
                 ],
               ),
             ],
