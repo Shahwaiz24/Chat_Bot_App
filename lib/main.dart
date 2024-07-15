948ae127605f2e09d595791a9d41fdc6c61005f7
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-07-15 03:59:33 -0700

    Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:chat_bot/Splash View/starting_view.dart';
+import 'package:chat_bot/firebase_options.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart'; // add this import
+import 'package:firebase_core/firebase_core.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
