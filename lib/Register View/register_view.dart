c3ada38ef0efd6683fd8c89214e33a931edb9fec
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-07-15 07:16:17 -0700

    Updated Code of Firebase

--- lib/Register View/register_view.dart
+++ lib/Register View/register_view.dart
@@ -7,6 +7,8 @@ import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
 import 'package:stacked/stacked.dart';
 
+bool isSentOtp = false;
+
 class RegisterView extends StatefulWidget {
   const RegisterView({super.key});
 
@@ -29,120 +31,141 @@ class _RegisterViewState extends State<RegisterView> {
         return Scaffold(
           resizeToAvoidBottomInset: false,
           backgroundColor: Utils.backgroundColor,
-          body: Stack(
-            children: [
-              Container(
-                height: screenHeight,
-                width: screenWidth,
-                child: const Image(
-                  image: AssetImage('assets/images/background_2.png'),
-                  fit: BoxFit.cover,
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: screenHeight * 0.045),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
+          body: isSentOtp == true
+              ? Stack(
                   children: [
-                    SizedBox(width: screenWidth * 0.05),
-                    InkWell(
-                      onTap: () {
-                        Navigator.pop(context);
-                      },
-                      child: Icon(
-                        Icons.arrow_back_outlined,
-                        size: screenHeight * 0.040,
-                        color: Utils.TextColor,
+                    Container(
+                      height: screenHeight,
+                      width: screenWidth,
+                      child: const Image(
+                        image: AssetImage('assets/images/background_2.png'),
+                        fit: BoxFit.cover,
                       ),
                     ),
+                    Center(
+                      child: CircularProgressIndicator(
+                        color: Utils.Purple,
+                      ),
+                    )
+                  ],
+                )
+              : Stack(
+                  children: [
+                    Container(
+                      height: screenHeight,
+                      width: screenWidth,
+                      child: const Image(
+                        image: AssetImage('assets/images/background_2.png'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(top: screenHeight * 0.045),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          SizedBox(width: screenWidth * 0.05),
+                          InkWell(
+                            onTap: () {
+                              Navigator.pop(context);
+                            },
+                            child: Icon(
+                              Icons.arrow_back_outlined,
+                              size: screenHeight * 0.040,
+                              color: Utils.TextColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Container(
+                        height: screenHeight * 0.500,
+                        width: screenWidth,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(screenWidth * 0.32),
+                            topRight: Radius.circular(screenWidth * 0.32),
+                          ),
+                          gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                              Utils.Purple.withOpacity(0.4),
+                              Utils.Pink.withOpacity(0.4)
+                            ],
+                          ),
+                        ),
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              SizedBox(
+                                height: screenHeight * 0.120,
+                              ),
+                              Text(
+                                'Add your Phone number',
+                                style: TextStyle(
+                                    color: Utils.TextColor,
+                                    fontSize: screenHeight * 0.020),
+                              ),
+                              SizedBox(
+                                height: screenHeight * 0.100,
+                              ),
+                              Padding(
+                                  padding: EdgeInsets.only(
+                                      left: screenWidth * 0.070,
+                                      right: screenWidth * 0.070),
+                                  child: Textfield(
+                                    false,
+                                    backColor: Utils.Purple,
+                                    Controller: PhoneNumberController,
+                                    hintColor: Utils.TextColor,
+                                    hintText: 'Enter your phone number',
+                                    screenHeight: screenHeight,
+                                    screenWidth: screenWidth,
+                                  )),
+                              SizedBox(
+                                height: screenHeight * 0.040,
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(
+                                    left: screenWidth * 0.180,
+                                    right: screenWidth * 0.180),
+                                child: Button(
+                                  text: 'Send Code',
+                                  ontap: () async {
+                                    
+                                    bool credential = await viewModel.OtpSent(
+                                        PhoneNumber: PhoneNumberController.text,
+                                        context: context,
+                                        view: GetOtpView);
+                                    if (credential == false) {
+                                    } else {}
+                                  },
+                                  screenHeight: screenHeight,
+                                  screenWidth: screenWidth,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: screenHeight * 0.400,
+                      left: screenWidth * 0.355,
+                      child: Container(
+                        height: screenHeight * 0.220,
+                        width: screenWidth * 0.260,
+                        child: const Image(
+                          image: AssetImage('assets/images/animIcon.png'),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    )
                   ],
                 ),
-              ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Container(
-                  height: screenHeight * 0.500,
-                  width: screenWidth,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(screenWidth * 0.32),
-                      topRight: Radius.circular(screenWidth * 0.32),
-                    ),
-                    gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        Utils.Purple.withOpacity(0.4),
-                        Utils.Pink.withOpacity(0.4)
-                      ],
-                    ),
-                  ),
-                  child: SingleChildScrollView(
-                    child: Column(
-                      children: [
-                        SizedBox(
-                          height: screenHeight * 0.120,
-                        ),
-                        Text(
-                          'Add your Phone number',
-                          style: TextStyle(
-                              color: Utils.TextColor,
-                              fontSize: screenHeight * 0.020),
-                        ),
-                        SizedBox(
-                          height: screenHeight * 0.100,
-                        ),
-                        Padding(
-                            padding: EdgeInsets.only(
-                                left: screenWidth * 0.070,
-                                right: screenWidth * 0.070),
-                            child: Textfield(
-                              false,
-                              backColor: Utils.Purple,
-                              Controller: PhoneNumberController,
-                              hintColor: Utils.TextColor,
-                              hintText: 'Enter your phone number',
-                              screenHeight: screenHeight,
-                              screenWidth: screenWidth,
-                            )),
-                        SizedBox(
-                          height: screenHeight * 0.040,
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(
-                              left: screenWidth * 0.180,
-                              right: screenWidth * 0.180),
-                          child: Button(
-                            text: 'Send Code',
-                            ontap: () async {
-                              await viewModel.OtpSent(
-                                  PhoneNumber: PhoneNumberController.text,
-                                  context: context,
-                                  view: GetOtpView);
-                            },
-                            screenHeight: screenHeight,
-                            screenWidth: screenWidth,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              Positioned(
-                top: screenHeight * 0.400,
-                left: screenWidth * 0.355,
-                child: Container(
-                  height: screenHeight * 0.220,
-                  width: screenWidth * 0.260,
-                  child: const Image(
-                    image: AssetImage('assets/images/animIcon.png'),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-              )
-            ],
-          ),
         );
       },
     );
--- lib/Register View/register_viewmodel.dart
+++ lib/Register View/register_viewmodel.dart
@@ -5,24 +5,31 @@ import 'package:stacked/stacked.dart';
 import 'package:flutter/material.dart';
 
 class RegisterViewmodel extends BaseViewModel {
+  stateRebuild({required bool isSenting}) {}
+
   OtpSent(
       {required String PhoneNumber, required context, required view}) async {
-    await FirebaseAuth.instance.verifyPhoneNumber(
-      verificationCompleted: (PhoneAuthCredential PhoneAuthCredential) {
-        print(PhoneAuthCredential.toString());
-      },
-      verificationFailed: (FirebaseAuthException exception) {
-        print(exception.toString());
-      },
-      codeSent: (String verificationid, int? token) {
-        print('PhoneNumber: ${PhoneNumber}');
-        Navigator.push(
-            context,
-            PageTransition(
-                type: PageTransitionType.bottomToTop, child: view()));
-      },
-      codeAutoRetrievalTimeout: (String verificationid) {},
-      phoneNumber: PhoneNumber,
-    );
+    try {
+      await FirebaseAuth.instance.verifyPhoneNumber(
+        verificationCompleted: (PhoneAuthCredential PhoneAuthCredential) {
+          print(PhoneAuthCredential.toString());
+        },
+        verificationFailed: (FirebaseAuthException exception) {
+          print(exception.toString());
+        },
+        codeSent: (String verificationid, int? token) {
+          print('PhoneNumber: ${PhoneNumber}');
+          Navigator.push(
+              context,
+              PageTransition(
+                  type: PageTransitionType.bottomToTop, child: view()));
+        },
+        codeAutoRetrievalTimeout: (String verificationid) {},
+        phoneNumber: PhoneNumber,
+      );
+    } on Exception catch (e) {
+      print("Execption: ${e.toString()}");
+      return false;
+    }
   }
 }
