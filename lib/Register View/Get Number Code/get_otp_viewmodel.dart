f27b56bee87d65a13af2b0889a426974bdf46276
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-07-15 07:03:00 -0700

    Updated Code

--- lib/Register View/Get Number Code/get_otp_viewmodel.dart
+++ lib/Register View/Get Number Code/get_otp_viewmodel.dart
@@ -1,12 +1,18 @@
 import 'package:chat_bot/Services/otp_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
-import 'package:page_transition/page_transition.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 import 'package:stacked/stacked.dart';
 
 class GetOtpViewmodel extends BaseViewModel {
-
-
-
-
+  bool otpCheck({required verificationId, required enteredCode}) {
+    try {
+      PhoneAuthCredential credential = PhoneAuthProvider.credential(
+          verificationId: verificationId, smsCode: enteredCode);
+      FirebaseAuth.instance.signInWithCredential(credential);
+    } catch (e) {
+      return false;
+    }
+    return true;
+  }
 }
--- lib/Register View/register_viewmodel.dart
+++ lib/Register View/register_viewmodel.dart
@@ -8,20 +8,21 @@ class RegisterViewmodel extends BaseViewModel {
   OtpSent(
       {required String PhoneNumber, required context, required view}) async {
     await FirebaseAuth.instance.verifyPhoneNumber(
-        verificationCompleted: (PhoneAuthCredential PhoneAuthCredential) {
-          print(PhoneAuthCredential.toString());
-        },
-        verificationFailed: (FirebaseAuthException exception) {
-          print(exception.toString());
-        },
-        codeSent: (String verificationid, int? token) {
-          print('PhoneNumber: ${PhoneNumber}');
-          Navigator.push(
-              context,
-              PageTransition(
-                  type: PageTransitionType.bottomToTop, child: view()));
-        },
-        codeAutoRetrievalTimeout: (String verificationid) {},
-        phoneNumber: PhoneNumber);
+      verificationCompleted: (PhoneAuthCredential PhoneAuthCredential) {
+        print(PhoneAuthCredential.toString());
+      },
+      verificationFailed: (FirebaseAuthException exception) {
+        print(exception.toString());
+      },
+      codeSent: (String verificationid, int? token) {
+        print('PhoneNumber: ${PhoneNumber}');
+        Navigator.push(
+            context,
+            PageTransition(
+                type: PageTransitionType.bottomToTop, child: view()));
+      },
+      codeAutoRetrievalTimeout: (String verificationid) {},
+      phoneNumber: PhoneNumber,
+    );
   }
 }
